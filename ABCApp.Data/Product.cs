ecff3aa138ec0a428bfe360c9bc972953983768a
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-06-25 12:03:17 +0100

    Add database context fro ABC

--- ABCApp.Data/Product.cs
+++ ABCApp.Data/Product.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ABCApp.Data
 {
+    [Table("Master_Product")]
     public class Product
     {
         public int ProductId { get; set; }
---
+++ ABCApp.Repo/ABCDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ABCApp.Data;
+using ABCApp.Data.Mappers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCApp.Repo
+{
+    public class ABCDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ABCDbContext(DbContextOptions<ABCDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);                            
+            modelBuilder.ApplyConfiguration(new ProducConfig());
+        }
+    }
+}
