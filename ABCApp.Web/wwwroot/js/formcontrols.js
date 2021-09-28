b43e27d723c0603154afedd8396e5783984101a2
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-09-29 00:36:46 +0100

    implemented related city dropdown for state dropdown select. added loading icon to dropdowns on ajax call for related data

--- ABCApp.Web/wwwroot/js/formcontrols.js
+++ ABCApp.Web/wwwroot/js/formcontrols.js
@@ -9,10 +9,12 @@
     //set to current date
     $('.datepicker').datepicker('pick');
 
-    $('#country').change(function () {
-        
-        $('#state').dropdown('clear');
-        $("#state").addClass('loading');
+    $('#country').change(function ()
+    {        
+        $('#state').dropdown('clear');        
+        $('#city').dropdown('clear');
+        $('#state').parent().addClass('loading'); 
+
         $.ajax({
             type: "post",
             url: "/Order/GetRegions",
@@ -20,7 +22,6 @@
             datatype: "json",
             traditional: true,
             success: function (data) {
-                
                 $("#state").empty()
                 $("#state").append("<option value='' selected='selected'>-- select state --</option>")
                 for (var i = 0; i < data.length; i++) {
@@ -28,9 +29,42 @@
                         + data[i].value + '">'
                         + data[i].text + '</option>')
                 }
-                
+            },
+            complete: function ()
+            {
+                setTimeout(() => {
+                    $('#state').parent().removeClass('loading');
+                }, 1000);                
             }
            
+        });
+    });
+
+    $('#state').change(function () {
+        
+        $('#city').dropdown('clear');        
+        $('#city').parent().addClass('loading');
+
+        $.ajax({
+            type: "post",
+            url: "/Order/GetCities",
+            data: { regionCode: $('#state').val() },
+            datatype: "json",
+            traditional: true,
+            success: function (data) {
+                $("#city").empty()
+                $("#city").append("<option value='' selected='selected'>-- select city --</option>")
+                for (var i = 0; i < data.length; i++) {
+                    $("#city").append('<option value="'
+                        + data[i].value + '">'
+                        + data[i].text + '</option>')
+                }
+            },
+            complete: function () {
+                setTimeout(() => {
+                    $('#city').parent().removeClass('loading');
+                }, 1000);
+            }
         });
     });
 
