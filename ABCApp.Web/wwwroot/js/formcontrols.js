58ff27272cab1a29002338a14015625d8ac7a49e
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-09-29 00:14:04 +0100

    implemented related state dropdown on country selection

--- ABCApp.Web/wwwroot/js/formcontrols.js
+++ ABCApp.Web/wwwroot/js/formcontrols.js
@@ -9,6 +9,31 @@
     //set to current date
     $('.datepicker').datepicker('pick');
 
+    $('#country').change(function () {
+        
+        $('#state').dropdown('clear');
+        $("#state").addClass('loading');
+        $.ajax({
+            type: "post",
+            url: "/Order/GetRegions",
+            data: { countryCode: $('#country').val() },
+            datatype: "json",
+            traditional: true,
+            success: function (data) {
+                
+                $("#state").empty()
+                $("#state").append("<option value='' selected='selected'>-- select state --</option>")
+                for (var i = 0; i < data.length; i++) {
+                    $("#state").append('<option value="'
+                        + data[i].value + '">'
+                        + data[i].text + '</option>')
+                }
+                
+            }
+           
+        });
+    });
+
 
 })
 
